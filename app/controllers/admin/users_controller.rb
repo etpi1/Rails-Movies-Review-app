9f241225cbe14f44f04637286dfeba45005edf3e
ethanpitt <ekid777@gmail.com>
2015-10-01 01:54:36 +0000

    Admin access added

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,2 +1,12 @@
 class Admin::UsersController < ApplicationController
+  before_filter :authorized_admin
+
+  def index
+    @user = User.all
+  end
+
+  def new
+    @user = User.new
+  end
+
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,17 @@ class ApplicationController < ActionController::Base
 
   protected
 
-  def restrict_access
-    if !current_user
-      flash[:alert] = "You must log in."
-      redirect_to new_session_path
-    end
-  end
-
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  helper_method :current_user
+  def restrict_access
+    redirect_to new_session_path, alert: "You must log in." unless current_user
+  end
 
+  def authorized_admin
+    redirect_to '/', alert: "You don't have the access." unless current_user && current_user.admin
+  end
+
+  helper_method :current_user
 end

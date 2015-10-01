0a12131542fad4a7e768430af98d194a299d2f08
ethanpitt <ekid777@gmail.com>
2015-10-01 22:36:15 +0000

    Admin edit and delete feature completed

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,8 +3,12 @@ class Admin::UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.page(params[:page]).per(3)
+    @users = User.page(params[:page]).per(10)
   end
+  
+  def show
+    @user = User.find(params[:id])
+  end 
 
   def new
     @user = User.new
@@ -15,12 +19,32 @@ class Admin::UsersController < ApplicationController
   end
 
   def create
-    @movie = Movie.new(movie_params)
-
-    if @movie.save
-      redirect_to movies_path
-    else
-      render :new
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to admin_users_path
     end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      redirect_to admin_users_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to admin_users_path
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :firstname, :lastname, :password, :password_confirmation, :admin)
+  end
 end
+
+
